4658d6bab480be5fbdef7713d763321945b6dad7
Rodolfo Rodrigues Pinheiro <rod_bma@hotmail.com>
2022-07-20 20:13:25 -0300

    Spring Initializr

---
+++ src/test/java/group/one/dronefeeder/DroneFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package group.one.dronefeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
