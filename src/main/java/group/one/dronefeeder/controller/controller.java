df04a5654bdc11e43629a9853b270a04a35d1f88
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-07-25 21:44:30 -0300

    feat: configuracoes adicionais

--- src/main/java/group/one/dronefeeder/controller/controller.java
+++ src/main/java/group/one/dronefeeder/controller/controller.java
@@ -1,5 +1,14 @@
 package group.one.dronefeeder.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class controller {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "HELLO WORLD";
+  }
 
 }
