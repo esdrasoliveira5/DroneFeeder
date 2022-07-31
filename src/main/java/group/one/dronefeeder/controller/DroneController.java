11636bc6374a39b751b02ef253a0673c6b2cb439
esionascimento <esionascimento@protonmail.com>
2022-07-31 05:23:15 -0400

    refactor: Controller drone, rotas configurados

--- src/main/java/group/one/dronefeeder/controller/DroneController.java
+++ src/main/java/group/one/dronefeeder/controller/DroneController.java
@@ -1,12 +1,13 @@
 package group.one.dronefeeder.controller;
 
-import group.one.dronefeeder.model.Drone;
 import group.one.dronefeeder.service.DroneService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import group.one.dronefeeder.model.Drone;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * DroneController.
@@ -16,9 +17,23 @@ public class DroneController {
   @Autowired
   private DroneService service;
 
-  @GetMapping("/drone")
+  @GetMapping("/")
+  public String testApplication() {
+    return "Teste";
+  }
+
+  @PostMapping("/droneteste")
+  public Drone testDrone(@RequestBody Drone drone) {
+    return drone;
+  }
+
+  @PostMapping("/drone")
   public Drone create(@RequestBody Drone drone) {
     return service.create(drone);
   }
 
+  @GetMapping("/drone")
+  public List<Drone> all() {
+    return service.findAll();
+  }
 }
