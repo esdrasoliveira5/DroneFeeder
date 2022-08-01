d51d93a95c7752db81cb5413d70e3b6a326e812a
esionascimento <esionascimento@protonmail.com>
2022-08-01 19:41:17 -0400

    create: DataError para exibir erros para usuario

---
+++ src/main/java/group/one/dronefeeder/exception/DataError.java
@@ -0,0 +1,17 @@
+package group.one.dronefeeder.exception;
+
+public class DataError {
+  private String error;
+
+  public DataError(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+}
