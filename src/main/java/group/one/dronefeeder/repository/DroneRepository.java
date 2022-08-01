4c9b7a51ec193bab7578bc75d4cd8211a2469bc7
esionascimento <esionascimento@protonmail.com>
2022-08-01 19:40:49 -0400

    create: adicionado DroneRepository->existsByMarca

--- src/main/java/group/one/dronefeeder/repository/DroneRepository.java
+++ src/main/java/group/one/dronefeeder/repository/DroneRepository.java
@@ -2,9 +2,10 @@ package group.one.dronefeeder.repository;
 
 import group.one.dronefeeder.model.Drone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DroneRepository extends JpaRepository<Drone, String> {
-
+public interface DroneRepository extends JpaRepository<Drone, Integer> {
+  boolean existsByMarca(String marca);
 }
