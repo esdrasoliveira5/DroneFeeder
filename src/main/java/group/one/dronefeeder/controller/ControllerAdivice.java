bc1f69d790656f8e616ad6567bea8d9387e7e0db
esionascimento <esionascimento@protonmail.com>
2022-08-02 22:01:51 -0400

    adicionado o primeiro teste verificando retorno create

--- src/main/java/group/one/dronefeeder/controller/ControllerAdivice.java
+++ src/main/java/group/one/dronefeeder/controller/ControllerAdivice.java
@@ -2,6 +2,7 @@ package group.one.dronefeeder.controller;
 
 import group.one.dronefeeder.exception.DataError;
 import group.one.dronefeeder.exception.DroneExistenteException;
+import group.one.dronefeeder.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,5 +16,10 @@ public class ControllerAdivice {
   @ExceptionHandler({DroneExistenteException.class})
   public ResponseEntity<DataError> handleConflict(RuntimeException err) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body(new DataError(err.getMessage()));
+  }
+
+  @ExceptionHandler({NotFoundException.class})
+  public ResponseEntity<DataError> handleNotFound(RuntimeException err) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new DataError(err.getMessage()));
   }
 }
