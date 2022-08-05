df480b61d3d091b72bbaec1ee2d5cd09f2e00206
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-08-05 11:27:42 -0300

    test: add testes to Video rout and Delivery Rout

--- src/test/java/group/one/dronefeeder/DroneFeederApplicationTests.java
+++ src/test/java/group/one/dronefeeder/DroneFeederApplicationTests.java
@@ -1,14 +1,17 @@
 package group.one.dronefeeder;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import group.one.dronefeeder.repository.DroneRepository;
-import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,76 +19,71 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import group.one.dronefeeder.model.Drone;
+import group.one.dronefeeder.repository.DroneRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DroneFeederApplicationTests {
-   @Autowired
-   private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-   @SpyBean
-   private DroneRepository droneRepository;
+  @SpyBean
+  private DroneRepository droneRepository;
 
-   @Captor
-   private ArgumentCaptor<Drone> droneCaptor;
+  @Captor
+  private ArgumentCaptor<Drone> droneCaptor;
 
-   @BeforeEach
-   public void setup() {
-      droneRepository.deleteAll();
-   }
+  @BeforeEach
+  public void setup() {
+    droneRepository.deleteAll();
+  }
 
-   @Test
-   @Order(1)
-   @DisplayName("1 - Deve adicionar um drone")
-   void deveAdicionarDrone() throws Exception {
-   final var drone = new Drone("leo12", "mar12");
-   mockMvc
-     .perform(post("/dronefeeder/drone")
-             .contentType(MediaType.APPLICATION_JSON)
-             .content(new ObjectMapper().writeValueAsString(drone)))
-     .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-           .andExpect(status().isOk())
-     .andExpect(jsonPath("$.marca").value(drone.getMarca()));
-   }
+  @Test
+  @Order(1)
+  @DisplayName("1 - Deve adicionar um drone")
+  void deveAdicionarDrone() throws Exception {
+    final var drone = new Drone("leo12", "mar12");
+    mockMvc
+        .perform(post("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(drone)))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+        .andExpect(jsonPath("$.marca").value(drone.getMarca()));
+  }
 
-   @Test
-   @Order(2)
-   @DisplayName("2 - Deve retornar todos os drones.")
-   void deveListarUmDrone() throws Exception {
-      final var drone1 = new Drone("leo12", "mar12");
-      final var drone2 = new Drone("leo1212", "mar1212");
-      final var drone3 = new Drone("Drone Esio", "mar echo esio");
+  @Test
+  @Order(2)
+  @DisplayName("2 - Deve retornar todos os drones.")
+  void deveListarUmDrone() throws Exception {
+    final var drone1 = new Drone("leo12", "mar12");
+    final var drone2 = new Drone("leo1212", "mar1212");
+    final var drone3 = new Drone("Drone Esio", "mar echo esio");
 
-      droneRepository.save(drone3);
-      droneRepository.save(drone2);
-      droneRepository.save(drone1);
+    droneRepository.save(drone1);
+    droneRepository.save(drone2);
+    droneRepository.save(drone3);
 
-      mockMvc.perform(get("/dronefeeder/drone")
-                      .contentType(MediaType.APPLICATION_JSON))
-              .andExpect(status().isOk())
-        .andExpect(jsonPath("$[0].marca").value(drone1.getMarca()))
+    mockMvc.perform(get("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$[0].marca").value(drone1.getMarca()))
         .andExpect(jsonPath("$[0].modelo").value(drone1.getModelo()))
-         .andExpect(jsonPath("$[1].marca").value(drone2.getMarca()))
-              .andExpect(jsonPath("$[1].modelo").value(drone2.getModelo()));
-   }
+        .andExpect(jsonPath("$[1].marca").value(drone2.getMarca()))
+        .andExpect(jsonPath("$[1].modelo").value(drone2.getModelo()));
+  }
 
-   @Test
-   @Order(3)
-   @DisplayName("3 - Deve retornar Um drone.")
-   void deveRetornarTempoGastoDeTodosEpisodiosMesmoSeNaoHouverEpisodio() throws Exception {
-      final var drone = droneRepository.save(new Drone("leo12", "mar12"));
-      mockMvc.perform(get("/dronefeeder/drone/" + drone.getId())
-          .contentType(MediaType.APPLICATION_JSON)
-          .contentType(MediaType.APPLICATION_JSON))
-          .andExpect(jsonPath("$.marca").value(drone.getMarca()))
-      .andExpect(jsonPath("$.modelo").value(drone.getModelo()));
-   }
+  @Test
+  @Order(3)
+  @DisplayName("3 - Deve retornar Um drone.")
+  void deveRetornarTempoGastoDeTodosEpisodiosMesmoSeNaoHouverEpisodio() throws Exception {
+    final var drone = droneRepository.save(new Drone("leo12", "mar12"));
+    mockMvc
+        .perform(get("/dronefeeder/drone/" + drone.getId()).contentType(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.marca").value(drone.getMarca()))
+        .andExpect(jsonPath("$.modelo").value(drone.getModelo()));
+  }
 
   @Test
   @Order(4)
@@ -93,11 +91,10 @@ class DroneFeederApplicationTests {
   void deveRetornarErroMesmaInfo() throws Exception {
     final var drone = droneRepository.save(new Drone("leo12", "mar12"));
     mockMvc
-            .perform(post("/dronefeeder/drone")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(new ObjectMapper().writeValueAsString(drone)))
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isConflict());
+        .perform(post("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(drone)))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isConflict());
   }
 
   @Test
--- src/test/java/group/one/dronefeeder/VideoTests.java
+++ src/test/java/group/one/dronefeeder/VideoTests.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -12,33 +13,47 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import group.one.dronefeeder.repository.DroneRepository;
+import group.one.dronefeeder.repository.VideoRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @RunWith(SpringRunner.class)
-class DeliveryTests {
+public class VideoTests {
   @Autowired
   private MockMvc mockMvc;
 
+  @SpyBean
+  private DroneRepository droneRepository;
+
+  @SpyBean
+  private VideoRepository videoRepository;
+
+  @BeforeEach
+  public void setup() {
+    droneRepository.deleteAll();
+    videoRepository.deleteAll();
+  }
+
   @Test
-  @DisplayName("1 - Deve retornar uma lista vazia quando nao houver delivery")
-  void deveRetornarListaVaziaQuandoNaoHouverDelivery() throws Exception {
-    final var resposta = mockMvc.perform(get("/dronefeeder/delivery"));
+  @DisplayName("1 - Deve retornar uma lista vazia quando nao houver videos")
+  void deveRetornarListaVaziaQuandoNaoHouverVideos() throws Exception {
+    final var resposta = mockMvc.perform(get("/dronefeeder/video"));
     resposta.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
         .andExpect(content().string(containsString("[]")));
   }
 
   @Test
-  @DisplayName("2 - Deve retornar uma lista vazia quando nao houver delivery")
-  void deveRetornarNotFoundQuandoNaoHouverDelivery() throws Exception {
-    final var resposta = mockMvc.perform(get("/dronefeeder/delivery/1"));
+  @DisplayName("2 - Deve retornar Not Found quando nao houver delivery")
+  void deveRetornarNotFoundQuandoNaoHouverVideo() throws Exception {
+    final var resposta = mockMvc.perform(get("/dronefeeder/video/1"));
     resposta.andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound())
-        .andExpect(content().string(containsString("Delivery Not Found!")));
+        .andExpect(content().string(containsString("Video Not Found!")));
   }
-
 }
