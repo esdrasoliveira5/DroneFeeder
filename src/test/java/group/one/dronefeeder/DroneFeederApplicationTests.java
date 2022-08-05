315c1658a8029f092f92e69ebd7d4117d391c3a9
esionascimento <esionascimento@protonmail.com>
2022-08-05 12:02:40 -0400

    fix: test drone

--- src/test/java/group/one/dronefeeder/DroneFeederApplicationTests.java
+++ src/test/java/group/one/dronefeeder/DroneFeederApplicationTests.java
@@ -6,14 +6,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import group.one.dronefeeder.repository.DroneRepository;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import group.one.dronefeeder.model.Drone;
-import group.one.dronefeeder.repository.DroneRepository;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,9 +29,6 @@ class DroneFeederApplicationTests {
 
   @SpyBean
   private DroneRepository droneRepository;
-
-  @Captor
-  private ArgumentCaptor<Drone> droneCaptor;
 
   @BeforeEach
   public void setup() {
@@ -48,10 +41,12 @@ class DroneFeederApplicationTests {
   void deveAdicionarDrone() throws Exception {
     final var drone = new Drone("leo12", "mar12");
     mockMvc
-        .perform(post("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON)
-            .content(new ObjectMapper().writeValueAsString(drone)))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-        .andExpect(jsonPath("$.marca").value(drone.getMarca()));
+      .perform(post("/dronefeeder/drone")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(drone)))
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.marca").value(drone.getMarca()));
   }
 
   @Test
@@ -62,39 +57,41 @@ class DroneFeederApplicationTests {
     final var drone2 = new Drone("leo1212", "mar1212");
     final var drone3 = new Drone("Drone Esio", "mar echo esio");
 
-    droneRepository.save(drone1);
-    droneRepository.save(drone2);
     droneRepository.save(drone3);
+    droneRepository.save(drone2);
+    droneRepository.save(drone1);
 
-    mockMvc.perform(get("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk()).andExpect(jsonPath("$[0].marca").value(drone1.getMarca()))
-        .andExpect(jsonPath("$[0].modelo").value(drone1.getModelo()))
-        .andExpect(jsonPath("$[1].marca").value(drone2.getMarca()))
-        .andExpect(jsonPath("$[1].modelo").value(drone2.getModelo()));
+    mockMvc.perform(get("/dronefeeder/drone")
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$[0].marca").value(drone1.getMarca()))
+      .andExpect(jsonPath("$[0].modelo").value(drone1.getModelo()))
+      .andExpect(jsonPath("$[2].marca").value(drone3.getMarca()))
+      .andExpect(jsonPath("$[2].modelo").value(drone3.getModelo()));
   }
 
   @Test
   @Order(3)
-  @DisplayName("3 - Deve retornar Um drone.")
-  void deveRetornarTempoGastoDeTodosEpisodiosMesmoSeNaoHouverEpisodio() throws Exception {
+  @DisplayName("3 - Deve retornar Um drone pelo id.")
+  void deveRetornarDronePeloId() throws Exception {
     final var drone = droneRepository.save(new Drone("leo12", "mar12"));
-    mockMvc
-        .perform(get("/dronefeeder/drone/" + drone.getId()).contentType(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.marca").value(drone.getMarca()))
-        .andExpect(jsonPath("$.modelo").value(drone.getModelo()));
+    mockMvc.perform(get("/dronefeeder/drone/" + drone.getId())
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.marca").value(drone.getMarca()))
+      .andExpect(jsonPath("$.modelo").value(drone.getModelo()));
   }
 
   @Test
   @Order(4)
-  @DisplayName("4 - Deve retornar erro 409, caso tente inserir mesma informação.")
+  @DisplayName("4 - Deve retornar erro, caso tente inserir mesma informação.")
   void deveRetornarErroMesmaInfo() throws Exception {
     final var drone = droneRepository.save(new Drone("leo12", "mar12"));
     mockMvc
-        .perform(post("/dronefeeder/drone").contentType(MediaType.APPLICATION_JSON)
-            .content(new ObjectMapper().writeValueAsString(drone)))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isConflict());
+      .perform(post("/dronefeeder/drone")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(drone)))
+      .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isConflict());
   }
 
   @Test
@@ -103,6 +100,26 @@ class DroneFeederApplicationTests {
   void deveRemoverDroneQuandoExistirNaBase() throws Exception {
     final var drone = droneRepository.save(new Drone("leo12", "mar12"));;
 
-    mockMvc.perform(delete("/dronefeeder/drone/" + drone.getId())).andExpect(status().isOk());
+    mockMvc.perform(delete("/dronefeeder/drone/" + drone.getId()))
+      .andExpect(status().isOk());
+  }
+
+  @Test
+  @Order(6)
+  @DisplayName("6 - Deve alterar propriedade de drone, por um id existente informado.")
+  void deveAterarConteudoDeUmaPropriedade() throws Exception {
+    final var drone = droneRepository.save(new Drone("leo12", "mar12"));;
+    MockHttpServletRequestBuilder builder =
+      MockMvcRequestBuilders.put("/dronefeeder/drone/" + drone.getId())
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .accept(MediaType.APPLICATION_JSON)
+        .content(getDroneInJson());
+    this.mockMvc.perform(builder)
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(jsonPath("$.marca").value("dronao"));
+  }
+
+  private String getDroneInJson() {
+    return "{\"marca\":\"dronao\"}";
   }
 }
